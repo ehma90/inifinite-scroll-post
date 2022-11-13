25b991b262f57bcf52863911adf155b4c173a4c6
Emmanuel Essien <ehmaessien@gmail.com>
2022-11-13 06:11:01 +0100

    Create inifinit scroll post blog

---
+++ script.js
@@ -0,0 +1,63 @@
+const postsContainer = document.getElementById("posts-container");
+const loading = document.querySelector(".loader");
+const filter = document.getElementById("filter");
+
+let limit = 5;
+let page = 1;
+
+// Fetch post from API
+async function getPost() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+
+  const data = res.json();
+
+  return data;
+}
+
+//Show post in DOM
+async function showPosts() {
+  const posts = await getPost();
+
+  posts.forEach((post) => {
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+    postElement.innerHTML = `
+        <div class="number">${post.id}</div>
+            <div class="post-info">
+                <h2 class="post-title">${post.title}</h2>
+                <p class="post-body">
+                    ${post.body}
+                </p>
+            </div>
+            `;
+        postsContainer.append(postElement)
+  });
+}
+
+// Show initial post
+showPosts();
+
+//Show loader and fetch more post 
+function showLoading(){
+    loading.classList.add('show');
+
+    setTimeout(() => {
+        loading.classList.remove('show')
+
+        setTimeout(() => {
+            page++
+            showPosts()
+        })
+    }, 1000)
+}
+
+
+window.addEventListener('scroll', () => {
+    const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+
+    if(scrollTop + clientHeight >= scrollHeight - 5){
+        showLoading();
+    }
+})
